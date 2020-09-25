8364ddb0621262ac718768b40cb08e29db5dc63f
shoaibhannure <shoaib_hannure@yahoo.com>
2020-09-26 01:59:36 +0530

    Verify Presence of an Element

---
+++ src/main/java/com/shoaib/selenium/VerifyElement.java
@@ -0,0 +1,36 @@
+package com.shoaib.selenium;
+
+/*
+ * 
+ * @Author- Shoaib Hannure
+ * 
+ */
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class VerifyElement {
+
+	public static void main(String[] args) {
+
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();// launch browser
+		driver.get("https://www.freshworks.com/");
+
+		WebElement platformLink = driver.findElement(By.linkText("Platform"));
+
+		if (platformLink.isDisplayed()) {
+			String text = platformLink.getText();
+			System.out.println(text);
+			platformLink.click();
+		}
+		else {
+			System.out.println("platform link not found");
+		}
+
+	}
+}
